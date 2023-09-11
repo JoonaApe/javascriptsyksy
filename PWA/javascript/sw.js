734df555079ae21994495479d5d9b3b91d469418
Joona Apell <joona.apell@metropolia.fi>
2023-09-11 10:22:30 +0300

    palautus

---
+++ PWA/javascript/sw.js
@@ -0,0 +1,20 @@
+const cacheName = 'hello-pwa';
+const filesToCache = ['/', 'pwa.html', 'css/pwa.css', 'javascript/main.js'];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', (e) => {
+   e.waitUntil(
+      caches.open(cacheName).then((cache) => {
+         return cache.addAll(filesToCache);
+      })
+   );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', (e) => {
+   e.respondWith(
+      caches.match(e.request).then((response) => {
+         return response || fetch(e.request);
+      })
+   );
+});
